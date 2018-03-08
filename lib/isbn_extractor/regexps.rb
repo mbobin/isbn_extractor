8836e2277e1c1b28a910e58050e5c61b679ddac1
Marius Bobin <marius.bobin@devacademy.ro>
2018-03-08 23:03:07 +0200

    update regexps

--- lib/isbn_extractor/regexps.rb
+++ lib/isbn_extractor/regexps.rb
@@ -9,6 +9,7 @@ module ISBNExtractor
     /\be-ISBN-13\W?\s?([\d\-\s]{13,20})\b/,
     /\bISBN-13\W?\s?\(pbk\)\W?\s?([\d\-\s]{13,20})\b/,
     /\bISBN-13\W?\s?\(electronic\)\W?\s?([\d\-\s]{13,20})\b/,
-    /\b(\d{3}-\d{1}-\d{3}-\d{5}-\d{1})\b/
+    /\b(\d{3}-\d{1}-\d{3}-\d{5}-\d{1})\b/,
+    /\b(\d{3}-\d{1}-\d{4}-\d{4}-\d{1})\b/,
   ]
 end
